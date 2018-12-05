8667837f79b1a8db06b46a99937eb2b7f92f6c5c
Sikandar Ali <sikandarali94@hotmail.com>
2018-12-06 08:38:08 +1100

    finished sec3 lec22 adding angular material to a project

---
+++ src/app/material.module.ts
@@ -0,0 +1,9 @@
+/* It is better to have a separate module for the Angular Material components we want to use. We then export this module to our app module.
+ */
+import {NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: []
+})
+export class MaterialModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+/* hammerjs is used by Angular Material to allow gesture support for touch screens.
+ */
+import 'hammerjs';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
