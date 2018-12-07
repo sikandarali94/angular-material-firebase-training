405f68fbde2366d6b10d5ee576fc526250d98b4a
Sikandar Ali <sikandarali94@hotmail.com>
2018-12-07 18:18:56 +1100

    finished sec3 lec24 our first angular material component - the button

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,10 @@
 /* It is better to have a separate module for the Angular Material components we want to use. We then export this module to our app module.
  */
 import {NgModule} from '@angular/core';
+import { MatButtonModule, MatIconModule } from '@angular/material';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatButtonModule, MatIconModule],
+  exports: [MatButtonModule, MatIconModule]
 })
 export class MaterialModule {}
