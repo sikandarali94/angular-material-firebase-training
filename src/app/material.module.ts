7b21b499249154bdced7be146e931187887da50e
Sikandar Ali <sikandarali94@hotmail.com>
2018-12-09 07:39:50 +1100

    finished sec3 lec27 working on the signup form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,10 @@
 /* It is better to have a separate module for the Angular Material components we want to use. We then export this module to our app module.
  */
 import {NgModule} from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
 })
 export class MaterialModule {}
