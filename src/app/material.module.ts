99b470c16dff67e3f5790fc6211a0dcafd6ba6cd
Sikandar Ali <sikandarali94@hotmail.com>
2018-12-12 15:21:30 +1100

    finished sec3 lec32 adding a datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,34 @@
 /* It is better to have a separate module for the Angular Material components we want to use. We then export this module to our app module.
  */
 import {NgModule} from '@angular/core';
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  /* We also need to provide a date adapter. We can either use MatNativeDateModule which supports US formatted date or MatMomentDateModule
+  which supports international date formats (Moment.js is needed as a dependency for this date adapter).
+   */
+  MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
 })
 export class MaterialModule {}
