c01fc9a848d2f76cdde64615ac31d3b1fab618b0
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-26 12:13:28 +1100

    finished sec5 lec61 routing & authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,17 +1,24 @@
+import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 
 import {User} from './user.model';
 import {AuthData} from './auth-data.model';
+import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: User;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 1000).toString()
     };
+    this.authSuccessfully();
   }
 
   login(authData: AuthData) {
@@ -19,12 +26,13 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 1000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
@@ -36,5 +44,12 @@ export class AuthService {
 
   isAuth() {
     return this.user != null;
+  }
+
+  /* We can even make a function private.
+   */
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 }
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
+import {AuthService} from '../../auth/auth.service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
+  isAuth = false;
   @Output() closeSidenav = new EventEmitter<void>();
+  authSubscription: Subscription;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(
+      (authStatus) => {
+        this.isAuth = authStatus;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
+  }
+
+  onLogout() {
+    this.onClose();
+    this.authService.logout();
   }
 
   onClose() {
