c8a32be08c043141384aebe9b5fb5c77ae4e4577
Sikandar Ali <sikandarali94@hotmail.com>
2019-01-30 12:26:24 +1100

    finished sec5 lec67 adding a form to the training component

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TrainingService} from '../training.service';
 import {Exercise} from '../exercise.model';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -16,8 +17,8 @@ export class NewTrainingComponent implements OnInit {
     this.availableExercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining() {
-    this.trainingService.startExercise();
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 }
